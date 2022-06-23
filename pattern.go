17ae81a2871f0823fe9df241c810285ef21ada80
strdh <strdh33@gmail.com>
2022-06-23 10:05:40 +0700

    .

--- pattern.go
+++ pattern.go
@@ -4,10 +4,84 @@ import "fmt"
 
 func main() {
 	n := 5
+
+	// for i := 1; i<=n; i++ {
+	// 	for j := i; j<=n; j++ {
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println("")
+	// }
+
+	// fmt.Println("")
+
+	// for i := 1; i<=n; i++ {
+	// 	for j := 1; j<=i; j++ {
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println("")
+	// }
+
+	// fmt.Println("")
+
+	// for i := 1; i<=n; i++ {
+	// 	for j := i; j<n; j++ {
+	// 		fmt.Print(" ")
+	// 	}
+
+	// 	for j := 1; j<=i; j++ {
+	// 		fmt.Print("*")
+	// 	}
+
+	// 	for j := 2; j<=i; j++ {
+	// 		fmt.Print("*")
+	// 	}
+
+	// 	fmt.Println("")
+	// }
+
+	// fmt.Println("")
+
+	// for i := 1; i<=n; i++ {
+	// 	for j := 2; j<=i; j++ {
+	// 		fmt.Print(" ")
+	// 	}
+
+	// 	for j := i; j<=n; j++ {
+	// 		fmt.Print("*")
+	// 	}
+
+	// 	for j := i; j<n; j++ {
+	// 		fmt.Print("*")
+	// 	}
+
+	// 	fmt.Println("")
+	// }
+
+	// fmt.Println("")
+
 	for i := 1; i<=n; i++ {
-		for j := i; j<=n; j++ {
-			fmt.Print("*")
+		for j := 1; j<=n*2; j++ {
+			if (i == 1 || i == n) || (j == 1 || j == n*2)  {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
 		}
 		fmt.Println("")
 	}
+
+	fmt.Println("")
+
+	for i := 1; i<=n; i++ {
+		for j := 1; j<=n*2; j++ {
+			if (i == 1 || i == n) || (j == 1 || j == n*2) {
+				fmt.Print("* ")
+			} else {
+				fmt.Print("  ")
+			}
+		}
+		fmt.Println("")
+	}
+
+
 }
